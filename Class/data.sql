3fd06eedef6b65af0b3ac3481b7338b457886193
Walid Saifi <saifiwalid21@gmail.com>
2023-12-14 17:04:23 +0100

    Update data.sql

--- Class/data.sql
+++ Class/data.sql
@@ -3,7 +3,7 @@ CREATE TABLE Utilisateur (
     id_utilisateur INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(255),
     prenom VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(255) ,
     mot_de_passe VARCHAR(255)
 );
 
