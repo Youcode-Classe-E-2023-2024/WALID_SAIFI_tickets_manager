7dfd91cb1eddb8d156f63beafdf541b946796a09
Walid Saifi <saifiwalid21@gmail.com>
2023-12-17 20:41:31 +0100

    ajouter des methode pour creation des tables

--- Class/dataBase.php
+++ Class/dataBase.php
@@ -43,6 +43,59 @@ class Database
         )";
         $this->conn->query($sql);
     }
+    private function createTicketTable()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS Ticket (
+            id_ticket INT PRIMARY KEY AUTO_INCREMENT,
+            titre VARCHAR(255),
+            description TEXT,
+            date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            priorite INT,
+            id_createur INT,
+            id_statut INT,
+            FOREIGN KEY (id_statut) REFERENCES Statut(id_statut),
+            FOREIGN KEY (id_createur) REFERENCES Utilisateur(id_utilisateur)
+        )";
+        $this->conn->query($sql);
+    }
+
+    private function createCommentaireTable()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS Commentaire (
+            id_commentaire INT PRIMARY KEY AUTO_INCREMENT,
+            contenu TEXT,
+            date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            id_utilisateur INT,
+            id_ticket INT,
+            FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+            FOREIGN KEY (id_ticket) REFERENCES Ticket(id_ticket)
+        )";
+        $this->conn->query($sql);
+    }
+
+    private function createAssignementTable()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS Assignement (
+            id_ticket INT,
+            id_assigne INT,
+            date_assignement TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (id_ticket) REFERENCES Ticket(id_ticket),
+            FOREIGN KEY (id_assigne) REFERENCES Utilisateur(id_utilisateur)
+        )";
+        $this->conn->query($sql);
+    }
+
+    private function createAffecterTagTable()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS AffecterTag (
+            id_ticket INT,
+            id_tag INT,
+            FOREIGN KEY (id_ticket) REFERENCES Ticket(id_ticket),
+            FOREIGN KEY (id_tag) REFERENCES Tag(id_tag)
+        )";
+        $this->conn->query($sql);
+    }
+
 
 
 }
