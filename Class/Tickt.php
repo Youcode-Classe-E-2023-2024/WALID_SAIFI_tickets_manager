654b1251bc503f4b194ebccd2bd8bee69d696b4b
Walid Saifi <saifiwalid21@gmail.com>
2023-12-18 12:01:12 +0100

    Update Tickt.php

--- Class/Tickt.php
+++ Class/Tickt.php
@@ -9,8 +9,12 @@
              public   $id_statut;
 
     public function __construct($titre,$description,$date_creation,$priorite,$id_createur,$id_statut){
-
-    
+        $this->titre=$titre;
+        $this->description=$description;
+        $this->date_creation=$date_creation;
+        $this->priorite=$priorite;
+        $this->id_createur=$id_createur;
+        $this->id_statut=$id_statut;
        }
 
     }
