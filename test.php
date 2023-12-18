d6b9472e5ea54635765134fab5657f134a1f50b2
Walid Saifi <saifiwalid21@gmail.com>
2023-12-18 14:02:06 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,13 @@
 <?php
 require_once("Class/utilisateur.php");
 
-
 $test = new utilisateur('NomSaifi', 'PrenomSaifi', 'saifi@example.com', 'motDePasse123');
-echo "--".$test->login('motDePsse123','saifi@example.com')."--";
 
+// Mettez à jour les propriétés de l'objet
+$test->nom = 'NouveauNom';
+$test->prenom = 'NouveauPrenom';
+$test->mot_de_passe = 'NouveauMotDePasse';
+
+// Appelez la fonction updateUser pour mettre à jour en base de données
+$test->updateUser();
 ?>
