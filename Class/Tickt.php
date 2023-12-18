0b2803f0f3d1146df2ebf40c109a62aa105b17d5
Walid Saifi <saifiwalid21@gmail.com>
2023-12-18 11:48:08 +0100

    Update Tickt.php

--- Class/Tickt.php
+++ Class/Tickt.php
@@ -1,9 +1,16 @@
 <?php
+  require_once("dataBase.php");
+       class Tickt extends Database{
+             public   $titre ;
+             public   $description;
+             public   $date_creation;
+             public   $priorite;
+             public   $id_createur;
+             public   $id_statut;
 
-       class Tickt{
+    public function __construct($titre,$description,$date_creation,$priorite,$id_createur,$id_statut){
 
-
-
-
-        
+    
        }
+
+    }
