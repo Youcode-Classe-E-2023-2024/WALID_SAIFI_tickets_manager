594a393601850d68a377b201fb8fda4036117926
Walid Saifi <saifiwalid21@gmail.com>
2023-12-18 15:14:58 +0100

    Update login.php

--- Vieux/login.php
+++ Vieux/login.php
@@ -1,4 +1,17 @@
+<?php
+require_once("../Class/utilisateur.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $mot_pass = $_POST['pass'];
+      $email = $_POST['email'];
+      $user  = new utilisateur();
+      if($user->login($mot_pass,$email) == 'yes'){
+             header("Location: Vieux/login.php");
+        }
+}
+?>
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -9,11 +22,11 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
   <style>
     body {
-      background-color: #f8f9fa; /* Choisir la couleur d'arrière-plan souhaitée */
+      background-color: #f8f9fa;
     }
     .card {
-      margin-top: 50px; /* Ajuster la marge supérieure pour centrer la carte */
-      background-color: #ffffff; /* Choisir la couleur d'arrière-plan de la carte */
+      margin-top: 50px; 
+      background-color: #ffffff; 
     }
   </style>
 </head>
@@ -30,11 +43,11 @@
           <form>
             <div class="mb-3">
               <label for="inputEmail" class="form-label">Email</label>
-              <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Entrez votre email">
+              <input type="email" name="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Entrez votre email">
             </div>
             <div class="mb-3">
               <label for="inputPassword" class="form-label">Mot de passe</label>
-              <input type="password" class="form-control" id="inputPassword" placeholder="Entrez votre mot de passe">
+              <input type="password" name = "pass" class="form-control" id="inputPassword" placeholder="Entrez votre mot de passe">
             </div>
             <button type="submit" class="btn btn-primary btn-block">Se connecter</button>
           </form>
