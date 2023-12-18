97858214d40781479340134c3db181753e164c51
Walid Saifi <saifiwalid21@gmail.com>
2023-12-18 15:15:02 +0100

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,6 @@ require_once("Class/utilisateur.php");
 
 
 $test = new utilisateur('NomSaifi', 'PrenomSaifi', 'saifi@example.com', 'motDePasse123');
-$test->nom = 'NouveauNom';
-$test->prenom = 'NouveauPrenom';
-$test->mot_de_passe = 'NouveauMotDePasse';
 
 
-$test->updateUser();
 ?>
