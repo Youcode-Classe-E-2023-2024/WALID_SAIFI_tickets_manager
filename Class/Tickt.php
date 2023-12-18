9e88f81839ba92aa2324948d2f11e5f6f5e66696
Walid Saifi <saifiwalid21@gmail.com>
2023-12-18 12:20:53 +0100

    Update Tickt.php

--- Class/Tickt.php
+++ Class/Tickt.php
@@ -1,20 +1,39 @@
 <?php
-  require_once("dataBase.php");
-       class Tickt extends Database{
-             public   $titre ;
-             public   $description;
-             public   $date_creation;
-             public   $priorite;
-             public   $id_createur;
-             public   $id_statut;
+require_once("dataBase.php");
 
-    public function __construct($titre,$description,$date_creation,$priorite,$id_createur,$id_statut){
-        $this->titre=$titre;
-        $this->description=$description;
-        $this->date_creation=$date_creation;
-        $this->priorite=$priorite;
-        $this->id_createur=$id_createur;
-        $this->id_statut=$id_statut;
-       }
+class Ticket extends Database
+{
+    public $id_ticket;
+    public $titre;
+    public $description;
+    public $date_creation;
+    public $priorite;
+    public $id_createur;
+    public $id_statut;
 
+    public function __construct($titre, $description, $priorite, $id_createur, $id_statut)
+    {
+        $this->titre = $titre;
+        $this->description = $description;
+        $this->priorite = $priorite;
+        $this->id_createur = $id_createur;
+        $this->id_statut = $id_statut;
     }
+
+
+    public function createTicket()
+    {
+        $query = "INSERT INTO Ticket (titre, description, priorite, id_createur, id_statut) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->getConnection()->prepare($query);
+        $stmt->bind_param("ssiii", $this->titre, $this->description, $this->priorite, $this->id_createur, $this->id_statut);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+ 
+
+}
