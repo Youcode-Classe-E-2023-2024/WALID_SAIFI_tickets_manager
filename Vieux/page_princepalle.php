c80b85275b1e57c7c7083e76b7333ecf4573ead9
Walid Saifi <saifiwalid21@gmail.com>
2023-12-19 23:19:03 +0100

    Update page_princepalle.php

--- Vieux/page_princepalle.php
+++ Vieux/page_princepalle.php
@@ -43,36 +43,24 @@
         <i class="bi bi-house-door-fill"></i>
         <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
       </div>
-      <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-        <i class="bi bi-bookmark-fill"></i>
-        <span class="text-[15px] ml-4 text-gray-200 font-bold">Ajouter Tickt</span>
-      </div>
+      <a href="ajouter_tichets.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+    <i class="bi bi-bookmark-fill"></i>
+    <span class="text-[15px] ml-4 text-gray-200 font-bold">Ajouter Ticket</span>
+        </a>
+
       <div class="my-4 bg-gray-600 h-[1px]"></div>
       <div
         class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
         onclick="dropdown()"
       >
         <i class="bi bi-chat-left-text-fill"></i>
-        <div class="flex justify-between w-full items-center">
-          <span class="text-[15px] ml-4 text-gray-200 font-bold">Chatbox</span>
-          <span class="text-sm rotate-180" id="arrow">
-            <i class="bi bi-chevron-down"></i>
-          </span>
-        </div>
-      </div>
-      <div
-        class="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
-        id="submenu"
-      >
-        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-          Social
-        </h1>
-        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-          Personal
-        </h1>
-        <h1 class="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">
-          Friends
-        </h1>
+        <a href="tikt_trater.php" class="flex justify-between w-full items-center">
+    <span class="text-[15px] ml-4 text-gray-200 font-bold">Ticht assiner a vous</span>
+    <span class="text-sm rotate-180" id="arrow">
+        <i class="bi bi-chevron-down"></i>
+    </span>
+</a>
+
       </div>
       <div
         class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
@@ -83,48 +71,44 @@
     </div>
     
 
-    <div class="max-w-4xl mx-auto">
-        <h1 class="text-2xl font-bold mb-4">Ticket Table</h1>
+    <div class="main-content ml-[300px] p-6">
+      <h2 class="text-2xl font-bold text-white mb-4">Liste des tickets</h2>
+  <table class="w-full bg-white border border-gray-300">
+    <thead>
+      <tr>
+        <th class="py-2 px-4 border-b">Titre</th>
+        <th class="py-2 px-4 border-b">Date</th>
+        <th class="py-2 px-4 border-b">Statut</th>
+        <th class="py-2 px-4 border-b">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Replace the following rows with your actual ticket data -->
+      <tr>
+        <td class="py-2 px-4 border-b">Ticket 1</td>
+        <td class="py-2 px-4 border-b">2023-01-01</td>
+        <td class="py-2 px-4 border-b">Open</td>
+        <td class="py-2 px-4 border-b">
+          <button class="bg-red-500 text-white px-2 py-1 rounded">Edit</button>
+        </td>
+      </tr>
+      <tr>
+        <td class="py-2 px-4 border-b">Ticket 2</td>
+        <td class="py-2 px-4 border-b">2023-02-01</td>
+        <td class="py-2 px-4 border-b">Closed</td>
+        <td class="py-2 px-4 border-b">
+          <button class="bg-red-500 text-white px-2 py-1 rounded">Edit</button>
+        </td>
+      </tr>
+      <!-- End of ticket rows -->
+    </tbody>
+  </table>
+</div>
 
-        <table class="min-w-full bg-white border border-gray-300">
-            <thead>
-                <tr>
-                    <th class="py-2 px-4 border-b">ID</th>
-                    <th class="py-2 px-4 border-b">Titre</th>
-                    <th class="py-2 px-4 border-b">Description</th>
-                    <th class="py-2 px-4 border-b">Date de création</th>
-                    <th class="py-2 px-4 border-b">Priorité</th>
-                    <th class="py-2 px-4 border-b">ID Créateur</th>
-                    <th class="py-2 px-4 border-b">ID Statut</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Les données de votre table seront ajoutées ici dynamiquement -->
-            </tbody>
-        </table>
-    </div>
+    
 
 
 
 
-    <script type="text/javascript">
-      	$(document).ready(function() {
-
-var table = $('#example').DataTable({
-    responsive: true
-  })
-  .columns.adjust()
-  .responsive.recalc();
-});
-      function dropdown() {
-        document.querySelector("#submenu").classList.toggle("hidden");
-        document.querySelector("#arrow").classList.toggle("rotate-0");
-      }
-      dropdown();
-
-      function openSidebar() {
-        document.querySelector(".sidebar").classList.toggle("hidden");
-      }
-    </script>
   </body>
 </html>
