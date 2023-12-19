a1d5d0aff6cf036aaec702ae6a2fb58108ca8495
Walid Saifi <saifiwalid21@gmail.com>
2023-12-19 22:22:19 +0100

    Update ajouter_tichets.php

--- Vieux/ajouter_tichets.php
+++ Vieux/ajouter_tichets.php
@@ -25,100 +25,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajouter un Ticket</title>
-    <style>
-        /* Add your custom CSS styles here */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            color: #333;
-            margin: 0;
-            padding: 0;
-        }
+    <!-- Add Tailwind CSS link -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
 
-        h2 {
-            color: #007BFF;
-        }
+<body class="bg-gray-200 font-sans">
 
-        form {
-            max-width: 400px;
-            margin: 20px auto;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
+    <form action="" method="post" class="max-w-md mx-auto mt-8 p-8 bg-white rounded shadow-md">
+        <h2 class="text-2xl mb-4 text-blue-500">Ajouter un Ticket</h2>
 
-        label {
-            display: block;
-            margin-bottom: 8px;
-            color: #555;
-        }
+        <label for="titre" class="block text-gray-600">Titre du Ticket:</label>
+        <input type="text" id="titre" name="titre" required
+            class="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500">
 
-        input,
-        textarea,
-        select {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 12px;
-            box-sizing: border-box;
-        }
+        <label for="description" class="block text-gray-600">Description:</label>
+        <textarea id="description" name="description" required
+            class="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
 
-        input[type="submit"] {
-            background-color: #007BFF;
-            color: #fff;
-            cursor: pointer;
-        }
+        <label for="priority" class="block text-gray-600">Priorité:</label>
+        <select id="priority" name="priority" required
+            class="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500">
+            <option value="" disabled selected>Choisissez une priorité</option>
+            <?php for ($i = 1; $i <= 10; $i++): ?>
+                <option value="<?= $i ?>"><?= $i ?></option>
+            <?php endfor; ?>
+        </select>
 
-        input[type="submit"]:hover {
-            background-color: #0056b3;
-        }
+        <label for="assigneur[]" class="block text-gray-600">Assigner à développeur :</label>
+        <select id="assigneur" name="assigneur[]" multiple
+            class="w-full px-3 py-2 mb-4 border rounded-md focus:outline-none focus:border-blue-500">
+            <?php
+            $conn = new Database();
+            $result = $conn->getConnection()->query("SELECT id_utilisateur, nom, prenom FROM Utilisateur");
+            while ($row = $result->fetch_assoc()) {
+                echo "<option value='" . $row['id_utilisateur'] . "'>" . $row['nom'] . " " . $row['prenom'] . "</option>";
+            }
+            ?>
+        </select>
 
-    </style>
-</head> 
-<body>
-
-<form action="" method="post">
-    <h2>Ajouter un Ticket</h2>
-    <label for="titre">Titre du Ticket:</label>
-    <input type="text" id="titre" name="titre" required><br>
-
-    <label for="description">Description:</label>
-    <textarea id="description" name="description" required></textarea><br>
-
-     <label for="priority">Priorité:</label>
-         <select id="priority" name="priority" required>
-  <option value="" disabled selected>Choisissez une priorité</option>
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">6</option>
-  <option value="7">7</option>
-  <option value="8">8</option>
-  <option value="9">9</option>
-  <option value="10">10</option>
-</select><br>
-
-
-
-    <label for="assigneur[]">Assigner à développeur :</label>
-    <select id="assigneur" name="assigneur[]" multiple>
-    <?php
-    $conn = new Database();
-    $result = $conn->getConnection()->query("SELECT id_utilisateur, nom, prenom FROM Utilisateur");
-    while ($row = $result->fetch_assoc()) {
-        echo "<option value='" . $row['id_utilisateur'] . "'>" . $row['nom'] . " " . $row['prenom'] . "</option>";
-    }
-    ?>
-</select><br>
-      <input type="submit" value="Ajouter Ticket">
-      </form>
-
+        <input type="submit" value="Ajouter Ticket"
+            class="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-700">
+    </form>
 
 </body>
+
 </html>
