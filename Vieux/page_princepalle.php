b077e9491672f918d9e7fb4bb400c94c5a00fe2b
Walid Saifi <saifiwalid21@gmail.com>
2023-12-20 23:02:35 +0100

    Update page_princepalle.php

--- Vieux/page_princepalle.php
+++ Vieux/page_princepalle.php
@@ -19,7 +19,7 @@ $userInfo = $data_user->getUserInfo_mon_Tickets($userId);
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-blue-900">
   <?php  require_once("nav_bar.php");  ?>
 <div class="w-10/12 ml-[300px] absolute right-0 " >
     <h2 class="text-2xl font-bold text-white mb-4">Liste des tickets</h2>
