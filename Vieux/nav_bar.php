3ffa21f1ca969cbd86ff4f6ba9286627a0176e7f
Walid Saifi <saifiwalid21@gmail.com>
2023-12-20 23:10:41 +0100

    Update nav_bar.php

--- Vieux/nav_bar.php
+++ Vieux/nav_bar.php
@@ -20,7 +20,7 @@
         </div>
 
         <!-- Home Link -->
-        <a href="home.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+        <a href="page_princepalle.php" class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
             <i class="bi bi-house-door-fill"></i>
             <span class="text-[15px] ml-4 text-gray-200 font-bold">Home</span>
         </a>
